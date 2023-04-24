fbaa8add9aec51694792532d3cba327fc08262df
KillerKing93 <131512446+KillerKing93@users.noreply.github.com>
2023-04-24 18:12:19 +0700

    Create OOP_Pembuatan-Fungsi.py

---
+++ Contoh-Object-Oriented-Programming/OOP_Pembuatan-Fungsi.py
@@ -0,0 +1,10 @@
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+    
+    def calculate_area(self):
+        return 3.14 * self.radius ** 2
+
+circle = Circle(5)
+area = circle.calculate_area()
+print(area)
